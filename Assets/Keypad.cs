1b98e17c140189f4ff7c45211c0f90dc110be540
JoelCefai <joel.cefai@gmail.com>
2022-05-18 13:04:46 +1000

    188/05

--- Assets/Keypad.cs
+++ Assets/Keypad.cs
@@ -1,96 +1,112 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
+using System.Collections;
 
 public class Keypad : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject keypadOB;
-    public GameObject hud;
-    public GameObject inv;
 
+    public string curPassword = "12345";
+    public string input;
+    public bool onTrigger;
+    public bool doorOpen;
+    public bool keypadScreen;
+    public Transform doorHinge;
 
-    public GameObject animateOB;
-    public Animator ANI;
-
-
-    public Text textOB;
-    public string answer = "12345";
-
-    public AudioSource button;
-    public AudioSource correct;
-    public AudioSource wrong;
-
-    public bool animate;
-
-
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        keypadOB.SetActive(false);
-
+        onTrigger = true;
     }
 
-
-    public void Number(int number)
+    void OnTriggerExit(Collider other)
     {
-        textOB.text += number.ToString();
-        button.Play();
+        onTrigger = false;
+        keypadScreen = false;
+        input = "";
     }
 
-    public void Execute()
+    void Update()
     {
-        if (textOB.text == answer)
+        if (input == curPassword)
         {
-            correct.Play();
-            textOB.text = "Right";
-
-        }
-        else
-        {
-            wrong.Play();
-            textOB.text = "Wrong";
+            doorOpen = true;
         }
 
-
-    }
-
-    public void Clear()
-    {
+        if (doorOpen)
         {
-            textOB.text = "";
-            button.Play();
+            var newRot = Quaternion.RotateTowards(doorHinge.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 250);
+            doorHinge.rotation = newRot;
         }
     }
 
-    public void Exit()
+    void OnGUI()
     {
-        keypadOB.SetActive(false);
-        inv.SetActive(true);
-        hud.SetActive(true);
-        player.GetComponent<FirstPersonController>().enabled = true;
-    }
-
-    public void Update()
-    {
-        if (textOB.text == "Right" && animate)
+        if (!doorOpen)
         {
-            ANI.SetBool("animate", true);
-            Debug.Log("its open");
+            if (onTrigger)
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "Press 'E' to open keypad");
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    keypadScreen = true;
+                    onTrigger = false;
+                }
+            }
+
+            if (keypadScreen)
+            {
+                GUI.Box(new Rect(0, 0, 320, 455), "");
+                GUI.Box(new Rect(5, 5, 310, 25), input);
+
+                if (GUI.Button(new Rect(5, 35, 100, 100), "1"))
+                {
+                    input = input + "1";
+                }
+
+                if (GUI.Button(new Rect(110, 35, 100, 100), "2"))
+                {
+                    input = input + "2";
+                }
+
+                if (GUI.Button(new Rect(215, 35, 100, 100), "3"))
+                {
+                    input = input + "3";
+                }
+
+                if (GUI.Button(new Rect(5, 140, 100, 100), "4"))
+                {
+                    input = input + "4";
+                }
+
+                if (GUI.Button(new Rect(110, 140, 100, 100), "5"))
+                {
+                    input = input + "5";
+                }
+
+                if (GUI.Button(new Rect(215, 140, 100, 100), "6"))
+                {
+                    input = input + "6";
+                }
+
+                if (GUI.Button(new Rect(5, 245, 100, 100), "7"))
+                {
+                    input = input + "7";
+                }
+
+                if (GUI.Button(new Rect(110, 245, 100, 100), "8"))
+                {
+                    input = input + "8";
+                }
+
+                if (GUI.Button(new Rect(215, 245, 100, 100), "9"))
+                {
+                    input = input + "9";
+                }
+
+                if (GUI.Button(new Rect(110, 350, 100, 100), "0"))
+                {
+                    input = input + "0";
+                }
+            }
         }
-
-
-        if(keypadOB.activeInHierarchy)
-        {
-            hud.SetActive(false);
-            inv.SetActive(false);
-            player.GetComponent<FirstPersonController>().enabled = false;
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-        }
-
     }
-
-
 }
